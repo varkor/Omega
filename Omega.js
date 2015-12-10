997f08812f4a82b1c225d06ceeac05ed0dc42637
varkor <github@varkor.com>
2015-12-10 20:47:44 +0000

    Added support for creating elements with IDs and classes
    As well as a collection of new methods such as .contains,
    .setAttribute, .withStyle, .remove, .rect and more.

--- Omega.js
+++ Omega.js
@@ -27,13 +27,27 @@ var Ω = (function () {
 			return from;
 		} if (from instanceof HTMLElement) {
 			this.element = from;
+		} else if (from instanceof Node) {
+			this.element = from;
 		} else if (typeof from === "string" || from instanceof String) {
 			var selector = from;
+			var id = selector.match(/#[a-z0-9-]+/);
+			selector = selector.replace(/#[a-z0-9-]+/, "");
+			var classes = selector.match(/\.[a-z0-9-]+/g);
+			selector = selector.replace(/\.[a-z0-9-]+/g, "");
 			var pseudoClasses = selector.match(/:[a-z-]+/g);
 			selector = selector.replace(/:[a-z-]+/g, "");
 			var attributes = selector.match(/\[[a-z]+(?:="[a-z0-9]+")?\]/g);
 			selector = selector.replace(/\[[a-z]+(?:="[a-z0-9]+")?\]/g, "");
 			this.element = document.createElement(selector);
+			if (id) {
+				this.element.id = id;
+			}
+			if (classes) {
+				classes.map(styleClass => styleClass.slice(1)).forEach(styleClass => {
+					this.element.classList.add(styleClass);
+				});
+			}
 			if (pseudoClasses) {
 				pseudoClasses.map(pseudoClass => pseudoClass.slice(1)).forEach(pseudoClass => {
 					switch (pseudoClass) {
@@ -75,11 +89,18 @@ var Ω = (function () {
 		}
 
 		// General Methods
+		this.setAttribute = (attribute, value) => {
+			this.element.setAttribute(attribute, value);
+			return this;
+		};
 		this.querySelector = selector => {
 			return Ω(this.element.querySelector(selector));
 		};
 		this.querySelectorAll = selector => {
 			return Ω(this.element.querySelectorAll(selector));
+		};
+		this.contains = object => {
+			return this.element.contains(Ω(object).element);
 		};
 		this.append = object => {
 			if (object !== null) {
@@ -91,14 +112,49 @@ var Ω = (function () {
 			Ω(object).append(this);
 			return this;
 		};
-		this.withText = text => {
-			this.element.appendChild(document.createTextNode(text));
+		this.follow = object => {
+			if (object !== null) {
+				this.element.parentNode.insertBefore(object.element, this.element.nextSibling);
+			}
+			return this;
+		}
+		this.withText = this.addText = text => {
+			if (`${text}`.length > 0) {
+				this.element.appendChild(document.createTextNode(text));
+			}
+			return this;
+		};
+		this.withStyle = this.addStyle = style => {
+			for (let attribute in style) {
+				this.element.style[attribute] = style[attribute];
+			}
 			return this;
 		};
 		this.clear = () => {
-			while (this.element.firstChild) {
-				this.element.removeChild(this.element.firstChild);
+			this.element.innerHTML = "";
+			return this;
+		};
+		this.hasClass = name => {
+			return this.element.classList.contains(name);
+		};
+		this.removeClass = name => {
+			this.element.classList.remove(name);
+			return this;
+		};
+		this.addClass = name => {
+			this.element.classList.add(name);
+			return this;
+		};
+		this.replaceClass = (original, replacement) => {
+			if (replacement !== undefined) {
+				return this.removeClass(original).addClass(replacement);
+			} else {
+				this.element.className = "";
+				return this.addClass(original);
 			}
+		};
+		this.remove = () => {
+			this.element.remove();
 			return this;
 		};
 		this.listenFor = (type, callback) => {
@@ -109,8 +165,9 @@ var Ω = (function () {
 			this.element.dispatchEvent(new Event(type));
 			return this;
 		};
-		this.onClick = callback => this.listenFor("click", event => callback());
+		this.onClick = callback => this.listenFor("click", event => callback(event));
 		this.click = () => this.trigger("click");
+		this.rect = () => this.element.getBoundingClientRect();
 
 		// Specific Methods
 		if (this.element instanceof HTMLTableElement) {
@@ -154,13 +211,23 @@ var Ω = (function () {
 			this.value = () => {
 				return this.element.value;
 			};
-			this.withValue = value => {
+			this.withValue = this.setValue = value => {
 				this.element.value = value;
 				return this;
 			};
 		}
 		if (this.element instanceof HTMLInputElement || this.element instanceof HTMLTextAreaElement) {
 			this.onInput = callback => this.listenFor("input", event => callback(this.value()));
+			this.onChange = callback => this.listenFor("change", event => callback());
+			this.focus = () => {
+				this.element.focus();
+				return this;
+			};
+			this.onBlur = callback => this.listenFor("blur", event => callback());
+			this.withSelection = this.setSelection = (start, end) => {
+				this.element.setSelectionRange(start, end);
+				return this;
+			};
 		};
 		if (this.element instanceof HTMLInputElement && ["checkbox", "radio"].indexOf(this.element.type) !== -1) {
 			this.checked = () => {
@@ -188,4 +255,14 @@ var Ω = (function () {
 	};
 
 	return Ω;
+}());
+
+var ß = (function () {
+	"use strict";
+
+	var ß = function (from) {
+		return Ω(document.createTextNode(from));
+	};
+
+	return ß;
 }());
